b917a676cdef733ccaf9a8bb4f45d89fa4074789
Shaquille Mandy <shaqmandy@gmail.com>
2024-03-14 23:14:22 -0300

    main and app file are now typed

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App.jsx'
+import App from './App'
 
-// eslint-disable-next-line react/no-deprecated
 ReactDOM.render(
 	<React.StrictMode>
 		<App />
