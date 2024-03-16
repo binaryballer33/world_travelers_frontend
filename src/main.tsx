e1420e9611490126a9afe7e168726d2b94a3fa4b
Shaquille Mandy <shaqmandy@gmail.com>
2024-03-16 17:37:16 -0300

    upgraded project to React 18, also updated other react dependencies to latest versions

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
 import './index.css'
 
-ReactDOM.render(
-	<Provider store={store}>
-		<React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+	<React.StrictMode>
+		<Provider store={store}>
 			<App />
-		</React.StrictMode>
-	</Provider>,
-	document.getElementById('root')
+		</Provider>
+	</React.StrictMode>
 )
